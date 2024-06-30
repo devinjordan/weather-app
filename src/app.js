9867e1eb9dfcbe65a3e602b8cbc5d461b321f45d
devinjordan <134808091+devinjordan@users.noreply.github.com>
2024-06-30 16:30:16 -0400

    Feat: Loader

--- src/app.js
+++ src/app.js
@@ -12,7 +12,12 @@ submitLocationRequest.addEventListener('click', async (event) => {
 
   const apiRequest = `https://api.weatherapi.com/v1/forecast.json?key=c8006c9d04664d00b48232348241905&q=${city}`;
 
+  const loader = document.getElementById('loader');
+  loader.style.display = 'block';
+
   const data = await weather.getData(apiRequest);
-  // const forecastData = await weather.getData(forecastRequest);
+  
+  loader.style.display = 'none';
+
   populate(data);
 });
